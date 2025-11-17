1caa3c4855ba5b7bc7b2d456599cde0de7d54784
rdkc4 <rdakic@protonmail.com>
2025-11-17 12:32:38 +0100

    Compiler documentation

--- compiler/compiler.hpp
+++ compiler/compiler.hpp
@@ -8,70 +8,60 @@
 #include "../common/abstract-syntax-tree/ASTProgram.hpp"
 #include "../common/intermediate-representation-tree/IRProgram.hpp"
 
+/** 
+ * @namespace Compiler
+ * @brief compilation module that orchestrates the process of compiling source code
+*/
 namespace Compiler {
-    
     enum class ExitCode { NO_ERR, LEXICAL_ERR, SYNTAX_ERR, SEMANTIC_ERR, IR_ERR, CODEGEN_ERR };
 
+    /** 
+     * @brief performs lexical analysis of the code
+     * @param lexer - reference to a lexer
+     * @returns LEXICAL_ERR if it captures any lexical errors, NO_ERR otherwise
+    */
     ExitCode lexicalAnalysis(Lexer& lexer);
+
+    /** 
+     * @brief performs syntax analysis of the code
+     * @param lexer - reference to a lexer
+     * @param astProgram - reference to the pointer of the AST program
+     * @returns SYNTAX_ERR if it captures any syntax errors, NO_ERR otherwise
+    */
     ExitCode syntaxAnalysis(Lexer& lexer, std::unique_ptr<ASTProgram>& astProgram);
+
+    /** 
+     * @brief performs semantic analysis of the code
+     * @param astProgram - reference to the pointer of the AST program
+     * @returns SEMANTIC_ERR if it captures any semantic errors, NO_ERR otherwise
+    */
     ExitCode semanticAnalysis(std::unique_ptr<ASTProgram>& astProgram);
+
+    /** 
+     * @brief performs transformation of the code from AST to IRT
+     * @param astProgram - reference to the pointer of the AST program
+     * @param irProgram - reference to the pointer of the IRT program
+     * @returns IR_ERR if it captures any errors, NO_ERR otherwise
+    */
     ExitCode transformASTToIRT(std::unique_ptr<ASTProgram>& astProgram, std::unique_ptr<IRProgram>& irProgram);
+
+    /** 
+     * @brief performs code generation
+     * @param irProgram - reference to the pointer of the IRT program
+     * @param output - path of the output file
+     * @returns CODEGEN_ERR if it fails to generate code, NO_ERR otherwise
+    */
     ExitCode generateCode(std::unique_ptr<IRProgram>& irProgram, const std::string_view output);
 
+    /** 
+     * @brief performs compilation of the code
+     *
+     * lexer -> parser -> analyzer -> intermediate-representation -> code-generation
+     * @param input - source code
+     * @param output - path of the output file
+     * @returns exit code depending on the result of the compilation
+    */
     ExitCode compile(std::string_view input, std::string_view output);
 };
 
 #endif
-
-/*
-
-    ExitCode handleIntermediateRepresentation(std::unique_ptr<ASTProgram>& astProgram, std::unique_ptr<IRProgram>& irProgram) {
-        try {
-            IntermediateRepresentation intermediateRepresentation {};
-            irProgram = intermediateRepresentation.formIR(astProgram.get());
-        } catch (const std::exception& e) {
-            std::cerr << std::format("\nForming Intermediate Representation: failed!\n{}\n", e.what());
-            return ExitCode::IR_ERR;
-        }
-        return ExitCode::NO_ERR;
-    }
-
-    ExitCode handleCodeGeneration(std::unique_ptr<IRProgram>& irProgram, const std::string_view output) {
-        try {
-            std::string filePath { std::string{ output } };
-            CodeGenerator codeGenerator{ filePath };
-            codeGenerator.generateCode(irProgram.get());
-        } catch (const std::exception& e) {
-            std::cerr << std::format("\nCode Generation: failed!\n{}\n", e.what());
-            return ExitCode::CODEGEN_ERR;
-        }
-        return ExitCode::NO_ERR;
-    }
-    
-}
-
-ExitCode Compiler::compile(std::string_view input, std::string_view output) {
-    Lexer lexer{ input };
-
-    // Handle lexical analysis
-    ExitCode code = handleLexicalAnalysis(input, lexer);
-    if (code != ExitCode::NO_ERR) return code;
-
-    // Handle parsing
-    std::unique_ptr<ASTProgram> astProgram;
-    code = handleParsing(lexer, astProgram);
-    if (code != ExitCode::NO_ERR) return code;
-
-    // Handle semantic analysis
-    code = handleSemanticAnalysis(astProgram);
-    if (code != ExitCode::NO_ERR) return code;
-
-    // Handle intermediate representation generation
-    std::unique_ptr<IRProgram> irProgram;
-    code = handleIntermediateRepresentation(astProgram, irProgram);
-    if (code != ExitCode::NO_ERR) return code;
-
-    // Handle code generation
-    return handleCodeGeneration(irProgram, output);
-}
-*/
