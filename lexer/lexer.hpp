84e65a76cbd6ab0fb5343e8752f73b8e60389a59
rdkc4 <rdakic@protonmail.com>
2025-11-19 23:09:24 +0100

    lexer documentation v1.1

--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -12,188 +12,188 @@
  * @brief Lexical analysis of the input
 */
 class Lexer{
-    public:
+public:
 
-        Lexer(std::string_view input);
+    Lexer(std::string_view input);
 
-        /**
-         * @brief translating input into a sequence of tokens
-         * @returns void 
-        */
-        void tokenize();
+    /**
+        * @brief translating input into a sequence of tokens
+        * @returns void 
+    */
+    void tokenize();
 
-        /**
-         * @brief increases nextTokenIdx
-         * @returns void
-        */
-        void next() noexcept;
+    /**
+        * @brief increases nextTokenIdx
+        * @returns void
+    */
+    void next() noexcept;
 
-        /**
-         * @brief gets the token that is next in line for parsing
-         * @returns const reference of the next token
-        */
-        const Token& peek() const noexcept;
+    /**
+        * @brief gets the token that is next in line for parsing
+        * @returns const reference of the next token
+    */
+    const Token& peek() const noexcept;
 
-        /**
-         * @brief gets the token that should be parsed at the moment
-         * @returns const reference of the current token
-        */
-        const Token& current() const noexcept;
+    /**
+        * @brief gets the token that should be parsed at the moment
+        * @returns const reference of the current token
+    */
+    const Token& current() const noexcept;
 
-        /** 
-         * @brief asserts that the tokenization has been completed
-         * @returns true if input has been tokenized, false otherwise
-        */
-        bool completedTokenization() const noexcept;
+    /** 
+        * @brief asserts that the tokenization has been completed
+        * @returns true if input has been tokenized, false otherwise
+    */
+    bool completedTokenization() const noexcept;
 
-        /** 
-         * @brief checks if any lexical error has been caught
-         * @returns false if there are no lexical errors, true otherwise
-        */
-        bool hasLexicalErrors() const noexcept;
+    /** 
+        * @brief checks if any lexical error has been caught
+        * @returns false if there are no lexical errors, true otherwise
+    */
+    bool hasLexicalErrors() const noexcept;
 
-        /**
-         * @brief prints all lexical errors
-         * @returns void
-        */
-        void showLexicalErrors() const; 
-        
-        /** 
-         * @brief prints all tokens
-         * @note debugging purposes
-         * @returns void
-        */
-        void printTokens() const noexcept;
-
-    private:
-        /// raw text that needs to be tokenized
-        std::string input;
-        
-        /// current position in the input
-        size_t position;
-        /// current line in the input
-        size_t lineNumber;
-        /// position at which previous line ended, used to determine the column of a token: column = position - prevLineLen
-        size_t prevLineLen;
-
-        /// index of a token that should be processed by the parser next
-        size_t nextTokenIdx;
-        /// vector of lexical errors
-        std::vector<std::string> exceptions;
+    /**
+        * @brief prints all lexical errors
+        * @returns void
+    */
+    void showLexicalErrors() const; 
     
-    protected:
-        /// vector of tokens generated from the input
-        std::vector<Token> tokens;
+    /** 
+        * @brief prints all tokens
+        * @note debugging purposes
+        * @returns void
+    */
+    void printTokens() const noexcept;
+
+private:
+    /// raw text that needs to be tokenized
+    std::string input;
     
-    private:
-        /**
-         * @brief increases position for 1
-         * @returns void
-        */
-        void updatePosition() noexcept;
+    /// current position in the input
+    size_t position;
+    /// current line in the input
+    size_t lineNumber;
+    /// position at which previous line ended, used to determine the column of a token: column = position - prevLineLen
+    size_t prevLineLen;
 
-        /**
-         * @brief increases the current position by a specified number of characters.
-         * @param n - the number of characters by which to increment the position.
-         * @returns void
-        */
-        void updatePosition(size_t n) noexcept;
+    /// index of a token that should be processed by the parser next
+    size_t nextTokenIdx;
+    /// vector of lexical errors
+    std::vector<std::string> exceptions;
 
-        /**
-         * @brief increases position for 1
-         *
-         * increases lineNumber for 1
-         *
-         * sets prevLineLen to position
-         * @returns void
-        */
-        void updateLine() noexcept;
+protected:
+    /// vector of tokens generated from the input
+    std::vector<Token> tokens;
 
-        /** 
-         * @brief pushes keyword or id to tokens
-         * @returns void
-        */
-        void pushID();
+private:
+    /**
+        * @brief increases position for 1
+        * @returns void
+    */
+    void updatePosition() noexcept;
 
-        /** 
-         * @brief pushes literal to tokens
-         * @param sign - flag that tells if literal is signed or not, default false
-         * @returns void
-        */
-        void pushLiteral(bool sign = false);
+    /**
+        * @brief increases the current position by a specified number of characters.
+        * @param n - the number of characters by which to increment the position.
+        * @returns void
+    */
+    void updatePosition(size_t n) noexcept;
 
-        /** 
-         * @brief pushes assignment to tokens
-         * @returns void
-        */
-        void pushAssignOperator();
+    /**
+        * @brief increases position for 1
+        *
+        * increases lineNumber for 1
+        *
+        * sets prevLineLen to position
+        * @returns void
+    */
+    void updateLine() noexcept;
 
-        /** 
-         * @brief pushes bitwise operator to tokens
-         * @returns void
-        */
-        void pushBitwiseOperator();
+    /** 
+        * @brief pushes keyword or id to tokens
+        * @returns void
+    */
+    void pushID();
 
-        /** 
-         * @brief pushes arithmetic operator to tokens
-         * @returns void
-        */
-        void pushAritOperator();
+    /** 
+        * @brief pushes literal to tokens
+        * @param sign - flag that tells if literal is signed or not, default false
+        * @returns void
+    */
+    void pushLiteral(bool sign = false);
 
-        /** 
-         * @brief pushes relational operator to tokens
-         * @returns void
-        */
-        void pushRelOperator();
+    /** 
+        * @brief pushes assignment to tokens
+        * @returns void
+    */
+    void pushAssignOperator();
 
-        /** 
-         * @brief checks if current sequence of characters is an assignment operator
-         * @returns true if sequence matches assign operator, false otherwise
-        */
-        bool isAssignOperator() const noexcept;
+    /** 
+        * @brief pushes bitwise operator to tokens
+        * @returns void
+    */
+    void pushBitwiseOperator();
 
-        /** 
-         * @brief checks if the current sequence of characters is a keyword
-         * @param value - const string representing id or keyword
-         * @returns true if sequence matches keyword, false otherwise
-        */
-        bool isKeyword(const std::string& value) const noexcept;
+    /** 
+        * @brief pushes arithmetic operator to tokens
+        * @returns void
+    */
+    void pushAritOperator();
 
-        /** 
-         * @brief checks if the current sequence of characters is a signed literal
-         * @returns true if sequence matches signed literal, false otherwise
-        */
-        bool isSignedLiteral() const noexcept;
+    /** 
+        * @brief pushes relational operator to tokens
+        * @returns void
+    */
+    void pushRelOperator();
 
-        /** 
-         * @brief checks if the current sequence of characters is an arithmetic operator
-         * @returns true if sequence matches arithmetic operator, false otherwise
-        */
-        bool isAritOperator() const;
+    /** 
+        * @brief checks if current sequence of characters is an assignment operator
+        * @returns true if sequence matches assign operator, false otherwise
+    */
+    bool isAssignOperator() const noexcept;
 
-        /** 
-         * @brief checks if the current sequence of characters is a bitwise operator
-         * @returns true if sequence matches bitwise operator, false otherwise
-        */
-        bool isBitwiseOperator() const;
+    /** 
+        * @brief checks if the current sequence of characters is a keyword
+        * @param value - const string representing id or keyword
+        * @returns true if sequence matches keyword, false otherwise
+    */
+    bool isKeyword(const std::string& value) const noexcept;
 
-        /** 
-         * @brief checks if the current sequence of characters is a relational operator
-         * @returns true if sequence matches relational operator, false otherwise
-        */
-        bool isRelOperator() const;
+    /** 
+        * @brief checks if the current sequence of characters is a signed literal
+        * @returns true if sequence matches signed literal, false otherwise
+    */
+    bool isSignedLiteral() const noexcept;
 
-        /** 
-         * @brief skips single line comment
-         * @returns void
-        */
-        void singleLineComment();
+    /** 
+        * @brief checks if the current sequence of characters is an arithmetic operator
+        * @returns true if sequence matches arithmetic operator, false otherwise
+    */
+    bool isAritOperator() const;
 
-        /** 
-         * @brief skips multi-line comment
-         * @returns void
-        */
-        void multiLineComment();
+    /** 
+        * @brief checks if the current sequence of characters is a bitwise operator
+        * @returns true if sequence matches bitwise operator, false otherwise
+    */
+    bool isBitwiseOperator() const;
+
+    /** 
+        * @brief checks if the current sequence of characters is a relational operator
+        * @returns true if sequence matches relational operator, false otherwise
+    */
+    bool isRelOperator() const;
+
+    /** 
+        * @brief skips single line comment
+        * @returns void
+    */
+    void singleLineComment();
+
+    /** 
+        * @brief skips multi-line comment
+        * @returns void
+    */
+    void multiLineComment();
 
 };
 
